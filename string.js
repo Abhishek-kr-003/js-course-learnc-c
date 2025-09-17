84aa65023bbbf225cfe26386f2b178bf3c48d419
Abhishek <abhikumar1756@gmail.com>
2025-09-17 13:00:33 +0000

    string concepts

---
+++ string.js
@@ -0,0 +1,10 @@
+const name = "Abhishek"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value")
+// This is the basic method to concatinate the string
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// this is modern and common way for string concat
+// we can also declare string as 
+const gameName = new String("Abhishek")
